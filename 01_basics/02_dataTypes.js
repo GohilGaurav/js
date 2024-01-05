6519d887f90b7932d052f0b144577af7211736cf
GohilGaurav <gauravgohil3597@gmail.com>
2024-01-05 13:34:54 +0000

    leaner about datatypes in java script

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Gaurav")
+
+
+let name = "Gaurav"
+let age = 2100000000000000000000000000000000000000000000000000000000000099999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof state); // undefined
+console.log(typeof null); // object
